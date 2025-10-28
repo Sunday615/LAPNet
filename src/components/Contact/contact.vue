cc55fc1e69602d7fbdf23d6d6846330d3d4df94a
Anuphong <anuphongthepvongsa@gmail.com>
2025-10-28 15:29:33 +0700

    Change contact file

--- src/components/Contact/contact.vue
+++ src/components/Contact/contact.vue
@@ -1,6 +1,37 @@
-<script setup lang="ts">
+<script setup>
 import navbartemplete from '../Navbar-footer/navbartemplete.vue';
 import ft from '../Navbar-footer/ft.vue';
+
+
+
+const sendEmail = () => {
+    const email = "info@laonet.com.la";
+    const subject = encodeURIComponent("สอบถามข้อมูลเพิ่มเติม");
+    const body = encodeURIComponent("สวัสดีครับ ผมอยากสอบถามเกี่ยวกับ...");
+    window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+};
+
+
+const openMap = () => {
+    const placeName = encodeURIComponent("Lao National Payment Network");
+    const mapUrl = `https://maps.app.goo.gl/TzSSbm5NQWCWNsrx7`;
+    window.open(mapUrl, "_blank");
+};
+
+
+
+const callPhone = () => {
+  const phoneNumber = "030 568 7610";
+  if (/Mobi|Android/i.test(navigator.userAgent)) {
+    // ถ้าอยู่บนมือถือ
+    window.location.href = `tel:${phoneNumber}`;
+    window.open(`tel:${phoneNumber}`);
+  } else {
+    alert(`กรุณาโทรเบอร์นี้ด้วยมือถือ: ${phoneNumber}`);
+  }
+};
+
+
 </script>
 
 
@@ -20,7 +51,8 @@ import ft from '../Navbar-footer/ft.vue';
                     <h1>Anywhere Anytime <br> Any Payment Chanel</h1>
                 </div>
                 <div class="infoboxcontainer">
-                    <div class="box1">
+
+                    <div class="box1" @click="openMap" style="cursor: pointer;"> 
                         <div class="flexboxcontainer">
                             <div class="icon">
                                 <i class="fa-solid fa-location-dot"></i>
@@ -33,7 +65,9 @@ import ft from '../Navbar-footer/ft.vue';
                         </div>
 
                     </div>
-                    <div class="box2">
+
+
+                    <div class="box2" style="cursor: pointer; " >
                         <div class="flexboxcontainer">
                             <div class="icon">
                                 <i class="fa-solid fa-phone"></i>
@@ -46,19 +80,23 @@ import ft from '../Navbar-footer/ft.vue';
                         </div>
 
                     </div>
-                    <div class="box3">
+
+                    <div class="box3" style="cursor:pointer;" @click="sendEmail">
                         <div class="flexboxcontainer">
-                            <div id="emailicon" class="icon" >
-                             <i class="fa-solid fa-inbox"></i>
+                            <div id="emailicon" class="icon">
+                                <i class="fa-solid fa-inbox"></i>
                             </div>
                             <div class="infotitle">
                                 <h2>Our Email</h2>
                                 <p>info@lapnet.com.la</p>
-                               
+
                             </div>
                         </div>
 
+
                     </div>
+
+
                 </div>
             </div>
             <div class="rightboxcontainer">
@@ -70,35 +108,39 @@ import ft from '../Navbar-footer/ft.vue';
                     <div class="boxinputname">
                         <input type="text" placeholder="Full Name *">
                     </div>
-                     <div class="boxinputemail">
+                    <div class="boxinputemail">
                         <input type="text" placeholder="Email Here *">
                     </div>
-                     <div class="boxinputmessage">
+                    <div class="boxinputmessage">
                         <Textarea placeholder="Your Message *"></Textarea>
                     </div>
                     <div class="btnsubmit">
-                        <button>Submit   Message</button>
+                        <button>Submit Message</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="maps">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3795.0006600873685!2d102.62610377608371!3d17.97870628301288!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x312467f0b486944f%3A0x84af2329498cb982!2sLao%20National%20Payment%20Network%20.%2C%20CO!5e0!3m2!1sth!2sla!4v1760583737341!5m2!1sth!2sla" width="1920" height="900" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3795.0006600873685!2d102.62610377608371!3d17.97870628301288!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x312467f0b486944f%3A0x84af2329498cb982!2sLao%20National%20Payment%20Network%20.%2C%20CO!5e0!3m2!1sth!2sla!4v1760583737341!5m2!1sth!2sla"
+            width="1920" height="900" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
-<ft></ft>
+    <ft></ft>
 </template>
 
 
 <style scoped>
-.maps{
+.maps {
     margin-top: 100px;
     width: 100%;
     height: 900px;
 
 
 }
-.btnsubmit button{
+
+.btnsubmit button {
     width: 100%;
     height: 100%;
     background-color: #000561;
@@ -108,86 +150,100 @@ import ft from '../Navbar-footer/ft.vue';
     border-bottom-right-radius: 20px;
     cursor: pointer;
 }
-.btnsubmit{
+
+.btnsubmit {
     width: 280px;
     height: 80px;
-    
-      margin-left: 70px;
+
+    margin-left: 70px;
     margin-top: 30px;
 }
-.boxinputmessage Textarea::placeholder{
+
+.boxinputmessage Textarea::placeholder {
     padding-top: 20px;
     padding-left: 20px;
     font-size: 20px;
 }
-.boxinputmessage Textarea{
+
+.boxinputmessage Textarea {
     width: 100%;
     height: 200px;
     background-color: white;
     border-top-left-radius: 15px;
     border-bottom-right-radius: 15px;
 }
-.boxinputmessage{
+
+.boxinputmessage {
     width: 80%;
     height: 200px;
- 
+
     margin-left: 70px;
     margin-top: 30px;
 }
-.boxinputemail input::placeholder{
+
+.boxinputemail input::placeholder {
     padding-left: 20px;
     font-size: 20px;
 }
-.boxinputemail input{
+
+.boxinputemail input {
     width: 100%;
     height: 100%;
     background-color: #fff;
     border-top-left-radius: 15px;
     border-bottom-right-radius: 15px;
 }
-.boxinputemail{
+
+.boxinputemail {
     width: 80%;
     height: 80px;
-  
+
     margin-left: 70px;
     margin-top: 30px;
 }
-.boxinputname input::placeholder{
+
+.boxinputname input::placeholder {
     padding-left: 20px;
     font-size: 20px;
 }
-.boxinputname input{
+
+.boxinputname input {
     width: 100%;
     height: 100%;
     background-color: #fff;
     border-top-left-radius: 15px;
     border-bottom-right-radius: 15px;
 }
-.boxinputname{
+
+.boxinputname {
     width: 80%;
     height: 80px;
 
     margin-left: 70px;
     margin-top: 80px;
 }
-.titlesendmail p{
+
+.titlesendmail p {
     font-size: 18px;
-    
+
     color: #fff;
 }
-.titlesendmail h1{
+
+.titlesendmail h1 {
     font-size: 50px;
     color: #fff;
 
 }
-.titlesendmail{
+
+.titlesendmail {
     width: 80%;
     height: 100px;
-     padding-top: 40px;
+    padding-top: 40px;
     margin-left: 70px;
 
 }
-.boxsendemail{
+
+.boxsendemail {
     width: 90%;
     height: 100%;
     margin-left: 10%;
@@ -195,6 +251,7 @@ import ft from '../Navbar-footer/ft.vue';
     border-top-left-radius: 60px;
     border-bottom-right-radius: 60px;
 }
+
 .infotitle p {
     font-size: 20px;
     line-height: 1.2;
@@ -243,20 +300,22 @@ import ft from '../Navbar-footer/ft.vue';
     border-radius: 10px;
     box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
 }
+
 .box2 {
-  
+
     width: 450px;
     height: 150px;
     margin-top: 20px;
-      border-radius: 10px;
+    border-radius: 10px;
     display: flex;
     box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
 }
+
 .box1 {
     margin-top: 40px;
     width: 450px;
     height: 150px;
-      border-radius: 10px;
+    border-radius: 10px;
     box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
     display: flex;
 }
@@ -276,7 +335,7 @@ import ft from '../Navbar-footer/ft.vue';
 .headercontact {
     width: 100%;
     height: 150px;
-  
+
     margin-top: 30px;
 }
 
@@ -322,7 +381,7 @@ import ft from '../Navbar-footer/ft.vue';
     border-radius: 40px;
     margin-top: 200px;
 
- 
+
 
     box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
 }
